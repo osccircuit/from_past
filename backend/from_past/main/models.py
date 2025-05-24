bcc598b0fff22c27aa2276497f836e7263906017
Egor Popov <popoveg18@gmail.com>
2025-05-25 00:28:23 +0300

    Fix potential problems

--- backend/from_past/main/models.py
+++ backend/from_past/main/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class Stack(models.Model):
     feature_name = models.CharField(max_length=50, unique=True,
                                     verbose_name='Название функции')
-    what_do = models.TextField(max_length=1500, verbose_name='Что делает')
-    how_did = models.TextField(max_length=1500, verbose_name='Как реализовано')
+    what_do = models.TextField(verbose_name='Что делает')
+    how_did = models.TextField(verbose_name='Как реализовано')
 
     def __str__(self):
         return str(self.feature_name).capitalize()
@@ -13,3 +13,4 @@ class Stack(models.Model):
         db_table='stack'
         verbose_name='Фича'
         verbose_name_plural='Фичи'
+        ordering = ['feature_name']
--- backend/from_past/main/views.py
+++ backend/from_past/main/views.py
@@ -6,6 +6,7 @@ from main.models import Stack
 class MainPageView(ListView):
     template_name = 'main/index.html'
     model = Stack
+    context_object_name = 'stacks'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
