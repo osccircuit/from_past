9e31db5a5ecf037364d31bae31367af41c74fb01
Egor Popov <popoveg18@gmail.com>
2025-05-24 23:59:54 +0300

    Final create main page

--- backend/from_past/main/models.py
+++ backend/from_past/main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Stack(models.Model):
+    feature_name = models.CharField(max_length=50, unique=True,
+                                    verbose_name='Название функции')
+    what_do = models.TextField(max_length=1500, verbose_name='Что делает')
+    how_did = models.TextField(max_length=1500, verbose_name='Как реализовано')
+
+    def __str__(self):
+        return str(self.feature_name).capitalize()
+
+    class Meta():
+        db_table='stack'
+        verbose_name='Фича'
+        verbose_name_plural='Фичи'
