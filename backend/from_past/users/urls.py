0395d8473b020a3bf657699ff1ca4431cfc5345e
Popov Egor <popoveg18@gmail.com>
2025-05-28 21:32:33 +0300

    Add sign in and registration urls

--- backend/from_past/users/urls.py
+++ backend/from_past/users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from users.views import RegistrationView
+from users.views import RegistrationView, SignInView 
 
 app_name = 'users'
 
 urlpatterns = [
-    path('registration/', RegistrationView.as_view(), name='registration')
+    path('registration/', RegistrationView.as_view(), name='registration'),
+    path('sign-in/', SignInView.as_view(), name='sign_in')
 ]
--- backend/from_past/users/views.py
+++ backend/from_past/users/views.py
@@ -8,3 +8,11 @@ class RegistrationView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Регистрация - FromPast'
         return context
+    
+class SignInView(TemplateView):
+    template_name = 'users/sign_in.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Вход - FromPast'
+        return context
