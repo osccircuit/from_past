905609d568cfa0197d62aa6921785451cab66b93
Popov Egor <popoveg18@gmail.com>
2025-06-10 21:00:13 +0300

    Add required login mixin

--- backend/from_past/letter/views.py
+++ backend/from_past/letter/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-# Create your views here.
-class NewLetterView(TemplateView):
+class NewLetterView(LoginRequiredMixin, TemplateView):
+    login_url = reverse_lazy('users:sign_in')
     template_name = "letter/letter_form.html"
 
     def get_context_data(self, **kwargs):
