e578a9e8448dabf77d057eb0eab407781f447e62
Egor Popov <popoveg18@gmail.com>
2025-06-14 11:28:36 +0300

    Add check letter date arrival

--- backend/from_past/letter/models.py
+++ backend/from_past/letter/models.py
@@ -7,12 +7,18 @@ from datetime import date
 def validate_today_date(value):
     if value == date.today():
        raise ValidationError('Нельзя установить сегодняшнюю дату') 
+
 class Letter(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь')
     for_who = models.CharField(max_length=200, verbose_name='Кому')
     from_who = models.CharField(max_length=200, verbose_name='От кого')
     letter_text = models.TextField(verbose_name='Текст письма')
     date_arrival = models.DateField(validators=[validate_today_date])
+
+    def check_date_arrival(self):
+        if self.date_arrival == date.today():
+            return True
+        return False
 
     def __str__(self):
         return f'Письмо в будущее №{self.pk}'
